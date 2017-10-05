3efb09ebaa228cd6dd461fcfb5073fc5cbb5296c
Chris Cardle <cdcardle@gmail.com>
2017-10-05 16:33:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ var katzDeliLine = []
 
 function takeANumber(katzDeliLine, customer) {
   katzDeliLine.push(customer)
-  return `Welcome, ` + customer + `. You are number ` + (katzDeliLine.indexOf(customer) + 1) + ` in line.`
+  return `Welcome, ${customer}. You are number ${katzDeliLine.indexOf(customer) + } in line.`
 }
 
 function nowServing(katzDeliLine) {
+<<<<<<< HEAD
   if (katzDeliLine.length < 1) {
     return `There is nobody waiting to be served!`
   } else return `Currently serving ` + katzDeliLine.shift() + `.`
@@ -21,4 +22,10 @@ function currentLine(line) {
       string = string + ` ${i+1}. ${line[i]},`
     }
   } return string.substr(0, (string.length - 1))
+=======
+  if (katzDeliLine.index < katzDeliLine.length) {
+    return `There is nobody waiting to be served!`
+  } else return `Currently serving ` + katzDeliLine[0] + `.`
+  return katzDeliLine.unshift
+>>>>>>> 71903c065ed6036e9e4486e5a91e79397847bc04
 }
